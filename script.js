0a09c1ee231a83bd0a589edbbd7d1a58cfdfa42b
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-20 14:11:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const list = document.getElementById("infi-list");
 function addListItems(count ){
 	for(let i= 0; i< count; i++){
 		const listItem = document.createElement("li");
-		listItem.textContent = 'item ${list.children.length+1};'
-		list.appendChild(listItem)
+		listItem.textContent = 'Item ${list.children.length+1};'
+		list.appendChild(listItem);
 	}
 }
 addListItems(10);
 list.addEventListener("scroll", function (){
-	if(this.scrollTop + this clientHeight >= this.scrollHeight)
+	if(this.scrollTop + this.clientHeight >= this.scrollHeight)
 addListItems(2);
 })
